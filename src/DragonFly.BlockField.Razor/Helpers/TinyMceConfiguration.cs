6ec321c01fff95428bf21c1dcd0e0bc388766948
usercode <2958488+usercode@users.noreply.github.com>
2022-09-24 22:03:30 +0200

    Added TinyMceConfiguration

---
+++ src/DragonFly.BlockField.Razor/Helpers/TinyMceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFly.BlockField.Razor.Helpers;
+
+public class TinyMceConfiguration
+{
+    public static Dictionary<string, object> Editor = new Dictionary<string, object>
+    {
+        { "menubar", true },
+        { "plugins", "link image code table lists autoresize" },
+        { "toolbar", "undo redo | forecolor | numlist bullist | bold italic | alignleft aligncenter alignright alignjustify | link image" }
+    };
+}
