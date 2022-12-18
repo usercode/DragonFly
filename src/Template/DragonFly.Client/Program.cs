b14e2fd1499f755d735a9c5286b002623f58144b
usercode <2958488+usercode@users.noreply.github.com>
2022-12-18 18:30:11 +0100

    Update package versions

--- src/Template/DragonFly.Client/Program.cs
+++ src/Template/DragonFly.Client/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<DragonFlyTemplate.Client.App>("app");
+builder.RootComponents.Add<DragonFly.Client.App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.AddDragonFlyClient()
