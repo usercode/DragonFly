df4f793b6270b890cb2fa823b5d4304ca9c59ab3
usercode <2958488+usercode@users.noreply.github.com>
2023-06-30 18:38:32 +0200

    Fixed unit test

--- src/DragonFly.Generator.Tests/GeneratorTest.cs
+++ src/DragonFly.Generator.Tests/GeneratorTest.cs
@@ -120,7 +120,6 @@ public class GeneratorTest
 
         Assert.Equal("Doe", content.GetString("Lastname"));
         Assert.Equal("John", content.GetString("Firstname"));
-        Assert.True(content.GetBool("IsActive"));
         //Assert.Equal(123, content.GetInteger("Value"));
         Assert.Equal("my-path", content.GetSlug("Slug"));
     }
